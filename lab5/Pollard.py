8ae5e403ae8740392cc7ba3fc85ab1e9d5c0ae02
alekseykrazhev <lkr37@mail.ru>
2022-05-19 17:11:32 +0300

    final changes

--- lab5/Pollard.py
+++ lab5/Pollard.py
@@ -1,26 +1,15 @@
-
-# The gcd function takes two integers and finds the greatest common divisor
-def gcd(a, b):
-    """
-        Euclid's algorithm for determining the greatest common divisor
-        """
-    while b != 0:
-        a, b = b, a % b
-    return a
+from RSA import gcd, fast_pow
 
 
-# The Pollard p-1 factorization algorithm
 def pollard_p(n: int, B: int) -> tuple[int, int]:
     """
     Finds a factors of n using the Pollard p-1 factorization algorithm.
     """
-    # start with b as 2 and user input upper bound
     b = 2
 
-    # run through all values between 2 and bound
     for i in range(2, B):
         # b is updated as a mod power
-        b = pow(b, i, n)
+        b = fast_pow(b, i, n)
         # d is gcd of b-1 and n
         d = gcd(b-1, n)
 
